30da03cd9d74536fb8850cc019f16bb58db4366d
Kritika-Panwar-151 <kritikapanwar.cs24@bmsce.ac.in>
2026-03-12 23:53:37 +0530

    added .env file

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,35 +1,35 @@
 import { useState } from "react";
+import { db } from "../services/firebase";
+import { collection, addDoc } from "firebase/firestore";
 
-function Form() {
-  const [input, setInput] = useState("");
+export default function Form() {
+  const [text, setText] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    alert("Submitted: " + input);
+
+    try {
+      await addDoc(collection(db, "submissions"), {
+        message: text,
+        createdAt: new Date()
+      });
+
+      alert("Stored in Firebase!");
+      setText("");
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit} className="bg-white p-6 rounded-xl shadow-md max-w-md">
-      <label className="block mb-2 font-medium">
-        Enter Data
-      </label>
-
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        className="border p-2 w-full rounded mb-4"
-        placeholder="Type something..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
       />
 
-      <button
-        type="submit"
-        className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
-      >
-        Submit
-      </button>
+      <button type="submit">Submit</button>
     </form>
   );
 }
-
-export default Form;
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2dgNgKnCnEPYi-EEW-REMdvxfTasvlMQ",
-  authDomain: "hackathon-project-5134f.firebaseapp.com",
-  projectId: "hackathon-project-5134f",
-  storageBucket: "hackathon-project-5134f.firebasestorage.app",
-  messagingSenderId: "876931286188",
-  appId: "1:876931286188:web:dbc992887418e08bcc8b09"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 
-// Firestore database
 export const db = getFirestore(app);
